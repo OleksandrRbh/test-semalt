e6120c05e7617de56fb1be5759f2a685276da7c8
OleksandrRbh <OleksandrRbh@gmail.com>
2019-11-25 19:31:28 +0200

    Hide burger from chart

--- source/js/chart.js
+++ source/js/chart.js
@@ -2,6 +2,14 @@ const chart = Highcharts.chart('dynamicsChart', {
     title: {
         text: ''
     },
+
+    exporting: {
+        buttons: {
+          contextButton: {
+            enabled: false
+          }
+        }
+    },
   
     xAxis: {
       categories: ['May 1', 'May 2', 'May 3', 'May 4', 'May 5', 'May 6', 'May 7', 'May 8', 'May 9', 'May 10', 'May 11', 'May 12', 'May 13', 'May 14', 'May 15', 'May 16', 'May 17', 'May 18', 'May 19', 'May 20', 'May 21', 'May 22', 'May 23', 'May 24', 'May 25', 'May 26', 'May 27', 'May 28', 'May 29', 'May 30', 'May 31']
